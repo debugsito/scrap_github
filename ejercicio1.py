af85ddb8492ba3682b3714dfa59a6db8192d3069
Your Name <you@example.com>
2025-08-24 00:01:15 -0500

    first version

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+import sys
+import numpy as np
+
+print("Usando Python desde:", sys.executable)
+print("Versión de Python:", sys.version)
+
+# Ejemplo con numpy
+a = np.array([1, 2, 3, 4])
+print("Array con numpy:", a)
+print("Suma:", np.sum(a))
